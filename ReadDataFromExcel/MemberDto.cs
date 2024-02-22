cac09baa389de6b35b82c7adcf286b24f0d0b6fc
Moh097 <mohammadmasri097@gmail.com>
2024-02-22 20:33:34 +0300

    start

--- ReadDataFromExcel/MemberDto.cs
+++ ReadDataFromExcel/MemberDto.cs
@@ -7,13 +7,19 @@ namespace ReadDataFromExcel
     {
         public string Id { get; set; }
         public string Name { get; set; }
+        public string Membership { get; set; }
+        public Image Photo { get; set; }
         public string Address { get; set; }
+        public string JoinDate { get; set; }
         public MemberDto() { }
-        public MemberDto(string id, string name , string address)
+        public MemberDto(string id, string name, Image photo, string membership, string address, string joinDate)
         {
             Id = id;
             Name = name;
+            Photo = photo;
+            Membership = membership;
             Address = address;
+            JoinDate = joinDate;
         }
     }
 }
